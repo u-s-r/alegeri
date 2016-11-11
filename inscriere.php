c257cb0b9a1b2c18e79bbf09926571fa3b7f325a
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-11-11 22:16:03 +0200

    Added signup function

---
+++ inscriere.php
@@ -0,0 +1,42 @@
+<?php
+require 'config.php';
+
+$url = 'https://script.google.com/macros/s/AKfycbwzaTnsfEmM32gfIf3QbVimFzHn803KrqsZ4b97b_iZEhum4ocL/exec';
+
+$data = array();
+
+if (!isset($_POST['tip'])) {
+  exit('false');
+}
+
+$data['sheetName']  = 'Sheet1';
+$data['tip']        = 'delegat' === $_POST['tip'] ? 'Delegat' : 'Reprezentant';
+$data['entry.3']    = $_POST['prenume'];
+$data['entry.4']    = $_POST['nume'];
+$data['entry.7']    = $_POST['email'];
+$data['entry.6']    = $_POST['telefon'];
+$data['cnp']        = $_POST['cnp'];
+$data['serie']      = $_POST['serie'];
+$data['numar']      = $_POST['numar'];
+$data['adresa']     = $_POST['adresa'];
+$data['entry.8']    = $_POST['regiune'];
+$data['entry.10']   = $_POST['tara'];
+$data['entry.25']   = $_POST['observatii'];
+$data['delegat']    = isset($_POST['delegat']);
+$data['confirmare'] = isset($_POST['confirmare']);
+$data['script']     = 'update';
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+var_dump($response);
+
+exit('true');
