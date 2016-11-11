48bab90ac6120e5a06608cfd847c2e2695005818
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-11-11 18:14:57 +0200

    Fixed forms validation

--- js/main.js
+++ js/main.js
@@ -102,20 +102,20 @@
   });
 
   $.validator.setDefaults({
-    highlight: function(element) {
-      $(element).closest('.form-group').addClass('has-error');
-    },
-    unhighlight: function(element) {
-      $(element).closest('.form-group').removeClass('has-error');
-    },
-    errorElement: 'span',
     errorClass: 'help-block',
+    errorElement: 'span',
     errorPlacement: function(error, element) {
-      if (element.parent('.input-group').length) {
+      if (element.parent('.control-label').length) {
         error.insertAfter(element.parent());
       } else {
         error.insertAfter(element);
       }
+    },
+    highlight: function(element) {
+      $(element).closest('.form-group, .checkbox').addClass('has-error');
+    },
+    unhighlight: function(element) {
+      $(element).closest('.form-group, .checkbox').removeClass('has-error');
     }
   });
 
