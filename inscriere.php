69540ca0e380ad0bc60a63b0f826ff31235a14c8
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-11-11 22:47:05 +0200

    Updated posted data

--- inscriere.php
+++ inscriere.php
@@ -3,42 +3,44 @@ require 'config.php';
 
 $url = 'https://script.google.com/macros/s/AKfycbwzaTnsfEmM32gfIf3QbVimFzHn803KrqsZ4b97b_iZEhum4ocL/exec';
 
-$data = array();
+header('Content-Type: application/json');
 
 if (!isset($_POST['tip'])) {
   exit('false');
 }
 
-$data['sheetName']  = 'Sheet1';
-$data['tip']        = 'delegat' === $_POST['tip'] ? 'Delegat' : 'Reprezentant';
-$data['entry.3']    = $_POST['prenume'];
-$data['entry.4']    = $_POST['nume'];
-$data['entry.7']    = $_POST['email'];
-$data['entry.6']    = $_POST['telefon'];
-$data['cnp']        = $_POST['cnp'];
-$data['serie']      = $_POST['serie'];
-$data['numar']      = $_POST['numar'];
-$data['adresa']     = $_POST['adresa'];
-$data['entry.8']    = $_POST['regiune'];
-$data['entry.9']    = $_POST['localitate'];
-$data['entry.10']   = $_POST['tara'];
-$data['entry.25']   = $_POST['observatii'];
-$data['delegat']    = isset($_POST['delegat']);
-$data['confirmare'] = isset($_POST['confirmare']);
-$data['entry.27']   = 'FormularAlegeri';
-$data['entry.28']   = 'update';
+$data = array(
+  'sheetName'  => 'Sheet1';
+  'tip'        => 'delegat' === $_POST['tip'] ? 'Delegat' : 'Reprezentant';
+  'entry.3'    => $_POST['prenume'];
+  'entry.4'    => $_POST['nume'];
+  'entry.7'    => $_POST['email'];
+  'entry.6'    => $_POST['telefon'];
+  'cnp'        => $_POST['cnp'];
+  'serie'      => $_POST['serie'];
+  'numar'      => $_POST['numar'];
+  'adresa'     => $_POST['adresa'];
+  'entry.8'    => $_POST['regiune'];
+  'entry.9'    => $_POST['localitate'];
+  'entry.10'   => $_POST['tara'];
+  'entry.25'   => $_POST['observatii'];
+  'delegat'    => isset($_POST['delegat']);
+  'confirmare' => isset($_POST['confirmare']);
+  'entry.27'   => 'FormularAlegeri';
+  'entry.28'   => 'update';
+);
 
 $ch = curl_init();
 
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 curl_setopt($ch, CURLOPT_URL, $url);
 
 $response = curl_exec($ch);
 
 curl_close($ch);
 
-var_dump($response);
-
-exit('true');
+exit(json_encode($response));
