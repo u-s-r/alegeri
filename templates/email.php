1d482e7a7edd51c01f3f04dce74c4793c0cd4524
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-11-14 21:36:37 +0200

    Updated site url

--- templates/email.php
+++ templates/email.php
@@ -130,7 +130,7 @@
                                 <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: separate; mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: auto;">
                                   <tbody>
                                     <tr>
-                                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;" valign="top" bgcolor="#3498db" align="center"><a href="https://www.facebook.com/sharer.php?u=https://alegeri.usr.ro/" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; border-color: #3498db;">Distribuie pe Facebook</a></td>
+                                      <td style="font-family: sans-serif; font-size: 14px; vertical-align: top; background-color: #3498db; border-radius: 5px; text-align: center;" valign="top" bgcolor="#3498db" align="center"><a href="https://www.facebook.com/sharer.php?u=https://usr.ro/alegeri/" target="_blank" style="display: inline-block; color: #ffffff; background-color: #3498db; border: solid 1px #3498db; border-radius: 5px; box-sizing: border-box; cursor: pointer; text-decoration: none; font-size: 14px; font-weight: bold; margin: 0; padding: 12px 25px; border-color: #3498db;">Distribuie pe Facebook</a></td>
                                     </tr>
                                   </tbody>
                                 </table>
